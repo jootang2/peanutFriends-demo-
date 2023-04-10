20812268d5b24fd26dad9096feff6cf4d1d555c8
joohwan9607 <joohwan9607@mobility42.io>
2023-04-11 01:40:44 +0900

    로그아웃

---
+++ src/main/java/com/sjh/peanutfriends_0324/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.sjh.peanutfriends_0324.config;
+
+import com.sjh.peanutfriends_0324.security.jwt.exception.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.*;
+
+// Spring Security 설정.
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationManagerConfig authenticationManagerConfig;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    // 가전제품 사용하는 것처럼 Spring Security라는 제품을 사용하는 것.
+    // JWT토큰을 인증을 한다. 그래서 인증에서 HttpSession을 사용하지 않는다.
+    // JWT의 장점을 찾아보자.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable() // 직접 id, password를 입력받아서 JWT토큰을 리턴하는 API를 직접 만든다.
+                .csrf().disable() // CSRF는 Cross Site Request Forgery의 약자. CSRF공격을 막기 위한 방법.
+                .cors() //.configurationSource(corsConfigurationSource())
+                .and()
+                .apply(authenticationManagerConfig)
+                .and()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // Preflight 요청은 허용한다. https://velog.io/@jijang/%EC%82%AC%EC%A0%84-%EC%9A%94%EC%B2%AD-Preflight-request
+                .requestMatchers(POST,"/api/members/signUp", "/api/members/login", "/api/members/refreshToken").permitAll()
+                .requestMatchers(GET,"/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(POST,"/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().hasAnyRole("USER", "ADMIN")
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+                .and()
+                .build();
+    }
+
+
+    // <<Advanced>> Security Cors로 변경 시도
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // config.setAllowCredentials(true); // 이거 빼면 된다
+        // https://gareen.tistory.com/66
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.setAllowedMethods(List.of("GET","POST","DELETE","PATCH","OPTION","PUT"));
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    // 암호를 암호화하거나, 사용자가 입력한 암호가 기존 암호랑 일치하는지 검사할 때 이 Bean을 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
